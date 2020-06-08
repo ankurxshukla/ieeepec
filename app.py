feb010b9b5cd7f2575398b9aaf754c71aae657bb
ankurshukla <ankurshukla1304@gmail.com>
2020-06-09 02:55:34 +0530

    Init frontend

--- app.py
+++ app.py
@@ -7,5 +7,12 @@ app = Flask(__name__)
 def func():
     return render_template('index.html')
 
+@app.route('/upload_resume', methods=['GET', 'POST'])
+def upload_resume():
+    if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
+
 if __name__ == "__main__":
     app.run(debug=True)
